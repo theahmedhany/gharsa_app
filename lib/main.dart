029db4603ec6129154263762140518a9194c2397
Ahmed Hany <a7medhanyshokry@gmail.com>
2025-07-06 11:20:14 +0300

    Gharsa App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'core/di/dependency_injection.dart';
 import 'core/helpers/constants.dart';
 import 'core/helpers/shared_pref_helper.dart';
-import 'my_app.dart';
-
 import 'core/routing/app_router.dart';
+import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initGetIt();
   await ScreenUtil.ensureScreenSize();
   await checkIfLoggedInUser();
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+
   runApp(MyApp(appRouter: AppRouter()));
 }
 
